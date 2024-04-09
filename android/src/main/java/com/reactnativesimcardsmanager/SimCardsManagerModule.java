29c68020fc491f4c20bef0086a01ed04d732b909
Wojtek <enigosi@gmail.com>
2024-04-09 09:51:09 +0200

    fixes

--- android/src/main/java/com/reactnativesimcardsmanager/SimCardsManagerModule.java
+++ android/src/main/java/com/reactnativesimcardsmanager/SimCardsManagerModule.java
@@ -13,6 +13,7 @@ import android.content.Intent;
 import android.telephony.euicc.DownloadableSubscription;
 import android.content.IntentFilter;
 import android.content.Context;
+import android.content.Intent.setPackage;
 import android.content.BroadcastReceiver;
 import android.telecom.PhoneAccountHandle;
 import android.telecom.TelecomManager;
@@ -28,7 +29,7 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.module.annotatfactions.ReactModule;
+import com.facebook.react.module.annotations.ReactModule;
 
 import java.util.List;
 
