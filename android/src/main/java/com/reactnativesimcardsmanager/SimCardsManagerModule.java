43b9f3acc8d30433c81e3006a8d14823be62523a
Wojtek <enigosi@gmail.com>
2024-05-15 18:48:35 +0200

    update

--- android/src/main/java/com/reactnativesimcardsmanager/SimCardsManagerModule.java
+++ android/src/main/java/com/reactnativesimcardsmanager/SimCardsManagerModule.java
@@ -155,10 +155,10 @@ public class SimCardsManagerModule extends ReactContextBaseJavaModule {
       EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_DETAILED_CODE,
     0 /* defaultValue*/);
 
-    int operationCode = intent.getIntExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_OPERATION_CODE);
-    int errorCode = intent.getIntExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_ERROR_CODE);
-    String smdxSubjectCode = intent.getStringExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_SMDX_SUBJECT_CODE);
-    String smdxReasonCode = intent.getStringExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_SMDX_REASON_CODE);
+    int operationCode = intent.getIntExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_OPERATION_CODE, 0);
+    int errorCode = intent.getIntExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_ERROR_CODE, 0);
+    String smdxSubjectCode = intent.getStringExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_SMDX_SUBJECT_CODE, "");
+    String smdxReasonCode = intent.getStringExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_SMDX_REASON_CODE, "");
 
     try {
       // Resolvable error, attempt to resolve it by a user action
